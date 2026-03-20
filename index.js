9251d18e72d54a28c833ff423087fc39d6562f13
giovanna rocha <giovannarochasantana4@gmail.com>
2026-03-20 16:15:10 -0300

    Geovanna

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/oi',(req,res) => {
+app.get('/Oi',(req,res) => {
     res.json({message:"Oi"})
 })
 
 app.listen(3000, () => {
     console.log("Server Ok")
+})
+
+app.get('/Geovanna',(req,res) => {
+    res.json({message:"oi Gee"})
 })
